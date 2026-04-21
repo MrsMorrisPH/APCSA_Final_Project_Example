6c5d57a02db3dc9a05eb2260a8e23d141b899b9f
MrsMorrisPH <kmorris3@sandi.net>
2026-04-21 12:06:27 -0700

    Formatted output to 2 decimal places

--- Main.java
+++ Main.java
@@ -33,8 +33,8 @@ public class Main {
         manager.addTransaction(t4);
 
         // test category total method
-        System.out.println("Total spent in Entertainment category: " + manager.calculateCategoryTotal(entertainment));
-        System.out.println("Total spent in Food category: " + manager.calculateCategoryTotal(food));
+        System.out.println("Total spent in Entertainment category: " + String.format("%.2f", manager.calculateCategoryTotal(entertainment)));
+        System.out.println("Total spent in Food category: " + String.format("%.2f", manager.calculateCategoryTotal(food)));
 
 
     }
